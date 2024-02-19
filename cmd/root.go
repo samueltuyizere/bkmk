a7f262e64b2e28609a923831d73f0e2e2ba42ae4
TUYIZERE Samuel <samueltuyizere0@gmail.com>
2024-02-19 14:13:50 +0200

    started working on the main command

---
+++ cmd/root.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2024 Samuel TUYIZERE sam@ejobu.rw
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "bkmk",
+	Short: "Bookmark and manage your CLI commands",
+	Long: `Bookmark and manage your CLI commands
+	To get help, run bkmk --help or bkmk -h`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.bkmk.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
